162bd1f9a580fdb51d81ad5bf4936d3a23da29ed
KlaiKlai <magallenmaynards@gmail.com>
2019-10-20 10:35:25 +0800

    neilVR

---
+++ Assets/BackMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class BackMenu : MonoBehaviour
+{
+    public Button BackButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackButton.onClick.AddListener(Back);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
